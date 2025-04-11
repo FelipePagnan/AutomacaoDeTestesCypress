5893b648b8b7087bb7e35c7e4fe8127312ffea12
Felipe Pagnan <luizfelipe130@outlook.com>
2025-04-10 22:56:50 -0300

    Aula introdutoria feita

--- 4. Fundamentos e Ferramentas de Automações de Testes/4. Automação de Testes Com Selenium WebDriver e Java/automation-pratice-selenium/src/test/java/qa/pagnan/automation/pratice/selenium/AppTest.java
+++ 4. Fundamentos e Ferramentas de Automações de Testes/4. Automação de Testes Com Selenium WebDriver e Java/automation-pratice-selenium/src/test/java/qa/pagnan/automation/pratice/selenium/AppTest.java
@@ -1,10 +1,31 @@
 package qa.pagnan.automation.pratice.selenium;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-
+	private WebDriver driver;
+	
+    @Test
+    public void helloSelenium() {
+        //https://www.selenium.dev/documentation/web/driver/getting_started/install_drivers/
+    	System.getProperty("webdriver.chrome.driver", "drivers/chromedriver");
+    	driver = new ChromeDriver();
+    	//driver.manage().window().maximize();
+    	driver.get("https://www.linkedin.com/in/felipepagnan/");
+    	
+    	String currentUrl = driver.getCurrentUrl();
+    	String expected = "https://www.linkedin.com/in/felipepagnan/";
+    	
+    	Assertions.assertEquals(expected, currentUrl);
+    	
+    	//driver.quit();
+    	
+    }
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
